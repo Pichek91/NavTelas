fcb919bc77cd77b4ca99ace01dbc215697bd7e77
= <=>
2024-05-16 17:54:08 -0300

    main

--- App.tsx
+++ App.tsx
@@ -2,17 +2,18 @@ import * as React from 'react';
 import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
 
 import { HomeScreen } from './src/pages/HomeScreen';
 import { NotificationsScreen } from './src/pages/NotificationsScreen';
 import { AdicionarEquipamento } from './src/pages/AdicionarEquipamento';
 
 
-const DrawerContetn = createDrawerNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer screenOptions={{draweStyle: { backgroudColor: 'pink', width: 240},}}>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="Notificações" component={NotificationsScreen} />
@@ -21,3 +22,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styleScreen = 
