dcfb892c208af9b24e5794483d36c13a20746d59
= <=>
2024-05-15 23:52:00 -0300

    main

--- App.tsx
+++ App.tsx
@@ -12,68 +12,16 @@ function TelaInicial() {
   );
 }
 
-function TelaConfig() 
-        return (
-            <View style={styles.container}>
-                <Image
-                source={require('./assets/icon.png')}
-                style={styles.logo}
-                />
-    
-                <Text style={styles.title}>20 Caracteres</Text>
-    
-                <View style={styles.area}>
-                    <Slider
-                    style={{height: 50}}
-                    minimumValue={6}
-                    maximumValue={20}
-                    maximumTrackTintColor="red"
-                    minimumTrackTintColor='red'
-                    thumbTintColor='blue'
-                    />
-                </View>
-    
-                <TouchableOpacity style={styles.button}>
-                <Text styles={styles.buttonText}>Gerar Senha</Text>
-                </TouchableOpacity>
-    
-            </View>
-        );
-    
-    const styles = StyleSheet.create({
-       container: {
-        flex: 1,
-        backgroundColor: '#F3F3FF',
-        alignItems: 'center',
-        justifyContent: 'center',
-       } ,
-       logo:{
-        marginBottom: 60
-       },
-       area:{
-        marginTop: 14,
-        marginBottom: 14,
-        width:"80%",
-        backgroundColor:"blue",
-        borderRadius: 8,
-        padding: 8,
-       },
-       button:{
-        backgroundColor:"blue",
-        width: "80%",
-        height: 50,
-        alignContent: "center",
-        justifyContent: "center",
-        borderRadius: 8,
-        marginBottom:18,
-       },
-        buttonText:{
-        color: "gold",
-        fontSize: 20,
-        
-       }
-    
-    });
+function TelaConfig() {
+return (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Configurações</Text>
+    <Image style={{flex:1, justifyContent:'center', marginBottom: '60'}}
+            source={require('./assets/icon.png')}
+            />
+  </View>
+);
+}
 
 function TelaGuia() {
   return (
